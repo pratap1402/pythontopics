bfc3add6f9f3a7fe7ef44ce73bb838d43a65de15
Sai pratap Jungili <saipratapjungili@Sais-MacBook-Pro.local>
2020-07-15 01:27:17 -0500

    added conditions_loops.py

---
+++ conditions_loops.py
@@ -0,0 +1,114 @@
+"""
+conditions and loops concepts
+"""
+# n=input("enter a number: ") # read input
+# print("type of n before conversion", type(n))
+# n = int(n) # type conversion
+# print("type of n after conversion", type(n))
+
+"""
+if
+1,2,3...positive
+"""
+# if n > 0:
+#     print('positive number')
+# print("Done!")
+
+"""
+if else
+even or odd
+2,4,6...is even
+1,3,5...is odd
+"""
+# if n%2 == 0:
+#     print(f'even')
+# else:
+#     print(f'odd')
+# print("Done!")
+
+"""
+if elif else
+fizzbuzz
+3,6,9,....is fizz
+5,10,15,...is buzz
+15,30,.....is fizzbuzz
+1,2,4......is not fizzbuzz
+"""
+# if n%3 == 0 and n%5 == 0:
+#     print(f'fizzbuzz')
+# elif n%3 == 0:
+#     print(f'fizz')
+# elif n%5 == 0:
+#     print(f'buzz')
+# else:
+#     print(n)
+# print("Done!")
+
+"""
+nested if else
+evenprime
+4,6,8...is even
+1,3,5,7...is odd
+2 is evenprime
+"""
+# if n%2 == 0:
+#     if n == 2:
+#         print(f'evenprime')
+#     else:
+#         print(f'even number')
+# else:
+#     print(f'odd')
+# print("Done!")
+
+"""
+while loop
+Program to add natural
+numbers up to n
+sum = 1+2+3+...+n
+"""
+# sum = 0
+# i = 1
+# while i <= n:
+#     sum = sum + i
+#     i = i+1    # update counter
+# print("The sum is", sum)
+# print("Done!")
+
+"""
+for loop
+"""
+# for i in range(n):
+#     print("prathap")
+# for i in "prathap":
+#     print(i)
+# for i in [1,2,3,4,5,6,7,8,9,10]:
+#     print(i)
+
+"""
+continue
+"""
+# for i in "prathap":
+#     if i == "t":
+#         continue
+#     print(i)
+# print("Done!")
+
+"""
+break
+"""
+# for a in "prathap":
+#     if a == "t":
+#         break
+#     print(a)
+# print("Done!")
+
+"""
+pass
+"""
+# for val in range(n):
+#     pass
+# print("Done!")
+#
+# if n%1 == 0:
+#     pass
+# print("Done!")
